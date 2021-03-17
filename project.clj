39241ebc824b0683fef8f68e048d139eba3a1e05
Michiel Borkent <michielborkent@gmail.com>
2021-03-17 19:53:42 +0100

    deps

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
         :url "https://github.com/babashka/babashka.pods"}
   :license {:name "EPL-1.0"
             :url "https://www.eclipse.org/legal/epl-1.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [nrepl/bencode "1.1.0"]
                  [cheshire "5.10.0"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
-                                    :username :env/babashka_nrepl_clojars_user
-                                    :password :env/babashka_nrepl_clojars_pass
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
                                     :sign-releases false}]]
-  :profiles {:test {:dependencies [[borkdude/sci "0.0.13-alpha.27"]]}})
+  :profiles {:test {:dependencies [[borkdude/sci "0.2.4"]]}})
