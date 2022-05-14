5aa5203a09b2799c65d440e31f681be93ef1f979
Wes Morgan <git@wesmorgan.me>
2022-05-14 10:53:21 -0600

    Add babashka/fs to project.clj and deps.edn
    Now that we're using it in the pods.impl ns

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
             :url "https://www.eclipse.org/legal/epl-1.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [nrepl/bencode "1.1.0"]
-                 [cheshire "5.10.0"]]
+                 [cheshire "5.10.0"]
+                 [babashka/fs "0.1.6"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
