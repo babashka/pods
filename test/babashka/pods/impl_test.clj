7c58f7bf9816938bf8790bbb8df1439d9542372f
lotuc <lotu.c@outlook.com>
2025-06-11 22:07:53 +0800

    Add tests for done invoke cleanup & unfulfilled invoke handling on pod quitting

--- test/babashka/pods/impl_test.clj
+++ test/babashka/pods/impl_test.clj
@@ -6,6 +6,24 @@
   (testing "resolve fn gets called when pod has EDN data readers"
     (let [resolved? (atom false)
           test-resolve (fn [_sym]
-                         (reset! resolved? true))]
-      (load-pod ["clojure" "-M:test-pod"] {:resolve test-resolve})
-      (is @resolved?))))
+                         (reset! resolved? true))
+          pod (load-pod ["clojure" "-M:test-pod"] {:resolve test-resolve})]
+      (is @resolved?)
+      (.destroy ^Process (:process pod)))))
+
+(deftest invoke-pod-test
+  (testing "invoke state (chan) being cleaned when done"
+    (let [pod (load-pod ["clojure" "-M:test-pod"] {:resolve identity})
+          _ (future (processor pod))]
+      (is (= 6 (invoke pod 'pod.test-pod/add-sync [4 2] {})))
+      (is (empty? @(:chans pod)))
+      (.destroy ^Process (:process pod))))
+
+  (testing "unfulfilled invokes throw on pod quitting"
+    (let [pod (load-pod ["clojure" "-M:test-pod"] {:resolve identity})
+          _ (future (processor pod))]
+      (try (invoke pod 'pod.test-pod/exit-1 [] {})
+           (catch Throwable e
+             (is (= "EOF" (ex-message e)))))
+      (is (empty? @(:chans pod)))
+      (.destroy ^Process (:process pod)))))
