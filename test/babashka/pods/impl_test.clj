8ab7f688c265942bec0e98a4fd8b11392fd77aa7
Michiel Borkent <michielborkent@gmail.com>
2025-06-12 15:22:11 +0200

    Fix #73 (Fulfilled invoke's state is not cleaned & unfulfilled invoke is not handled on pod quitting) (#75)
    * Add a exit-1 fn to test-pod for pod's accidentaly quitting test
    
    * Add tests for done invoke cleanup & unfulfilled invoke handling on pod quitting
    
    * Handle all pending invoke on pod exit
    
    * Cleanup invoke chan on fulfilled
    
    * Fix test
    
    ---------
    
    Co-authored-by: lotuc <lotu.c@outlook.com>

--- test/babashka/pods/impl_test.clj
+++ test/babashka/pods/impl_test.clj
@@ -1,11 +1,30 @@
 (ns babashka.pods.impl-test
-  (:require [clojure.test :refer :all]
-            [babashka.pods.impl :refer :all]))
+  (:require
+   [babashka.pods.impl :refer [invoke load-pod processor]]
+   [clojure.test :refer [deftest is testing]]))
 
 (deftest load-pod-test
   (testing "resolve fn gets called when pod has EDN data readers"
     (let [resolved? (atom false)
           test-resolve (fn [_sym]
-                         (reset! resolved? true))]
-      (load-pod ["clojure" "-M:test-pod"] {:resolve test-resolve})
-      (is @resolved?))))
+                         (reset! resolved? true))
+          pod (load-pod ["clojure" "-M:test-pod"] {:resolve test-resolve})]
+      (is @resolved?)
+      (.destroy ^Process (:process pod)))))
+
+(deftest invoke-pod-test
+  (testing "invoke state (chan) being cleaned when done"
+    (let [pod (load-pod ["clojure" "-M:test-pod"] {:resolve identity})
+          _ (future (processor pod))]
+      (is (= 6 (invoke pod 'pod.test-pod/add-sync [4 2] {})))
+      (is (empty? @(:chans pod)))
+      (.destroy ^Process (:process pod))))
+
+  (testing "unfulfilled invokes throw on pod quitting"
+    (let [pod (load-pod ["clojure" "-M:test-pod"] {:resolve identity})
+          _ (future (processor pod))]
+      (try (invoke pod 'pod.test-pod/exit-1 [] {})
+           (catch Throwable e
+             (is (= "Pod quit unexpectedly" (ex-message e)))))
+      (is (empty? @(:chans pod)))
+      (.destroy ^Process (:process pod)))))
